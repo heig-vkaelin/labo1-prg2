0b5003bb7a8a3f88fa14317035cc333052f58515
Valentin Kaelin <kalanehd@gmail.com>
2021-04-22 12:08:09 +0200

    feat: some work on labo1

--- Labo1/listes_dynamiques.c
+++ Labo1/listes_dynamiques.c
@@ -12,5 +12,140 @@
  -----------------------------------------------------------------------------------
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "listes_dynamiques.h"
 
+Liste *initialiser() {
+	Liste *liste = (Liste *) malloc(sizeof(Liste));
+	if (liste) {
+		liste->tete = NULL;
+		liste->queue = NULL;
+	}
+	return liste;
+}
+
+bool estVide(const Liste *liste) {
+	return liste->tete == NULL && liste->queue == NULL;
+}
+
+size_t longueur(const Liste *liste) {
+	// TODO: est-elle utile à l'interne ?
+	size_t longueur = 0;
+	Element *temp = liste->tete;
+	while (temp) {
+		++longueur;
+		temp = temp->suivant;
+	}
+	return longueur;
+}
+
+Element *creerElement(const Info *info) {
+	Element *nouvelElement = (Element *) malloc(sizeof(Element));
+	if (nouvelElement) {
+		nouvelElement->info = *info;
+		nouvelElement->precedent = NULL;
+		nouvelElement->suivant = NULL;
+	}
+
+	return nouvelElement;
+}
+
+Status insererEnTete(Liste *liste, const Info *info) {
+	Element *nouvelElement = creerElement(info);
+	if (!nouvelElement) {
+		return MEMOIRE_INSUFFISANTE;
+	}
+
+	if (estVide(liste)) {
+		liste->tete = nouvelElement;
+		liste->queue = nouvelElement;
+		return OK;
+	}
+
+	liste->tete->precedent = nouvelElement;
+	nouvelElement->suivant = liste->tete;
+	liste->tete = nouvelElement;
+
+	return OK;
+}
+
+Status insererEnQueue(Liste *liste, const Info *info) {
+	Element *nouvelElement = creerElement(info);
+	if (!nouvelElement) {
+		return MEMOIRE_INSUFFISANTE;
+	}
+
+	if (estVide(liste)) {
+		liste->tete = nouvelElement;
+		liste->queue = nouvelElement;
+		return OK;
+	}
+
+	liste->queue->suivant = nouvelElement;
+	nouvelElement->precedent = liste->queue;
+	liste->queue = nouvelElement;
+
+	return OK;
+}
+
+void afficher(const Liste *liste, Mode mode) {
+	// TODO: refactor avec une fonction prenant une fonction en params
+	printf("[");
+	Element *temp;
+	if (mode == FORWARD) {
+		temp = liste->tete;
+		while (temp) {
+			printf("%d", temp->info);
+			if ((temp = temp->suivant)) {
+				printf(", ");
+			}
+		}
+	} else {
+		temp = liste->queue;
+		while (temp) {
+			printf("%d", temp->info);
+			if ((temp = temp->precedent)) {
+				printf(", ");
+			}
+		}
+	}
+	printf("]\n");
+}
+
+Status supprimerEnTete(Liste *liste, Info *info) {
+	if (estVide(liste)) {
+		return LISTE_VIDE;
+	}
+
+	*info = liste->tete->info;
+
+	// TODO: 1) quand tete = queue, il faut aussi delete la queue
+	// TODO 2) Si longueur == 1 : queue devient aussi la tete
+	liste->tete = liste->tete->suivant;
+	free(liste->tete->precedent);
+	liste->tete->precedent = NULL;
+
+	return OK;
+}
+
+Status supprimerEnQueue(Liste *liste, Info *info) {
+	if (estVide(liste)) {
+		return LISTE_VIDE;
+	}
+
+	return OK;
+}
+
+void supprimerSelonCritere(Liste *liste,
+									bool (*critere)(size_t position, const Info *info)) {
+
+}
+
+void vider(Liste *liste, size_t position) {
+
+}
+
+bool sontEgales(const Liste *liste1, const Liste *liste2) {
+
+}
--- Labo1/main.c
+++ Labo1/main.c
@@ -17,6 +17,34 @@
 #include "listes_dynamiques.h"
 
 int main(void) {
-	printf("Hello, World!\n");
+	// 1. Initialiser
+	Liste *liste1 = initialiser();
+	Liste *liste2 = initialiser();
+
+	// 2. Ajouter des éléments à la tête
+	const Info INFO_1 = 1;
+	const Info INFO_2 = 2;
+	const Info INFO_3 = 3;
+	insererEnTete(liste1, &INFO_1);
+	insererEnTete(liste1, &INFO_2);
+	insererEnTete(liste1, &INFO_3);
+	insererEnQueue(liste1, &INFO_2);
+
+	printf("Longueur liste1: %zu\n", longueur(liste1));
+	printf("Longueur liste2: %zu\n", longueur(liste2));
+
+	afficher(liste1, FORWARD);
+	afficher(liste1, BACKWARD);
+
+	// 3. Supprimer des éléments
+	Info ancienneTete = 0;
+	supprimerEnTete(liste1, &ancienneTete);
+	supprimerEnTete(liste1, &ancienneTete);
+	supprimerEnTete(liste1, &ancienneTete);
+	supprimerEnTete(liste1, &ancienneTete);
+	supprimerEnTete(liste1, &ancienneTete);
+	printf("Element supprime: %d\n", ancienneTete);
+	afficher(liste1, FORWARD);
+
 	return EXIT_SUCCESS;
 }
